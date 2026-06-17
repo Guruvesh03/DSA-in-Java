a871d85695ba1c70af0a7d5ec4763a33c8e2f2bc
Guruvesh03 <bachhavguruvesh@gmail.com>
2026-06-17 10:02:27 +0530

    feat(BitManipulation):Updated the PowerOfTwo problem code.

--- BitManipulation/PowerOfTwo.java
+++ BitManipulation/PowerOfTwo.java
@@ -4,16 +4,26 @@ import java.util.*;
 
 public class PowerOfTwo {
     public static void main(String[] args) {
-        System.out.println("Enter the value of the power: ");
+        System.out.print("Enter the value of the power: ");
         Scanner sc = new Scanner(System.in);
         int power = sc.nextInt();
+        System.out.print("\nEnter the number you want to check if its power of two:");
+        int number = sc.nextInt();
 
         System.out.println("2^" + power + " = " + findPower(power));
+        System.out.println("Is the number power of two: " + checkPower(number));
 
 
     }
 
     public static int findPower(int power) {
         return 1 << power;
+    }
+
+    public static boolean checkPower(int number) {
+        if ((number & (number - 1)) == 0) {
+            return true;
+        }
+        return false;
     }
 }
